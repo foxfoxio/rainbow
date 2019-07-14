8a7472ca3d9a668256e0db3c5ae4904f906afbcb
katopz <katopz@gmail.com>
2019-07-14 22:08:21 +0700

    Resizec onchage

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import styled from 'styled-components'
 import domtoimage from 'dom-to-image'
 import { saveAs } from 'file-saver'
@@ -24,22 +24,40 @@ export const Facebookz = styled.div`
 
   .event
   {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 500px;
+    margin: auto;
+    
     width: 500px;
     height: 262px;
 
     input[type='text'] {
       background-color:rgb(0,0,0,0);
-      border-width:0;
+      border-width: 0;
       font-size: 32px;
-      width: inherit;
+      width: fit-content;
+      height: fit-content;
       text-shadow: -1px 0 red, 1px 0 cyan;
       color: white;
+      text-align: left;
     }
   }
 `
 
 export default () => {
+  const [eventTitle] = useState('Python for Data Science')
+  const [size, setSize] = useState(eventTitle.length)
   const eventRef = useRef(null)
+
+  const onChange = e => {
+    const newSize = Math.max(e.target.value.length, 1)
+
+    e.target.size = newSize
+    setSize(newSize)
+  }
+
   const onClick = () => {
     const node = eventRef.current
 
@@ -64,7 +82,7 @@ export default () => {
       <h1>Event</h1>
       <small>500x262 pixels</small>
       <div className='event' ref={eventRef}>
-        <input type='text' defaultValue='Python for Data Science' />
+        <input type='text' size={size} onChange={onChange} defaultValue={eventTitle} />
       </div>
       <button onClick={onClick}>SAVE</button>
       <br />
